9e1047afeea3c439f67395a8727d0dd9cb42dc44
Drazox <68930610+drazoxXD@users.noreply.github.com>
2023-07-10 16:16:10 +0200

    Initial upload

---
+++ client.lua
@@ -0,0 +1,85 @@
+CreateThread(function()
+	while true do
+		if IsPedArmed(cache.ped, 4) ~= false then
+			local ped = cache.ped
+			if IsPedShooting(ped) and not IsPedDoingDriveby(ped) then
+				local _, wep = GetCurrentPedWeapon(ped)
+				_, cAmmo = GetAmmoInClip(ped, wep)
+				if recoils[wep] and recoils[wep] ~= 0 then
+					-- luacheck: ignore
+					local tv = 0
+					if GetFollowPedCamViewMode() ~= 4 then
+						repeat
+							Wait(0)
+							local p = GetGameplayCamRelativePitch()
+							SetGameplayCamRelativePitch(p + 0.1, 0.2)
+							tv += 0.1
+						until tv >= recoils[wep]
+					else
+						repeat
+							Wait(0)
+							local p = GetGameplayCamRelativePitch()
+							if recoils[wep] > 0.1 then
+								SetGameplayCamRelativePitch(p + 0.6, 1.2)
+								tv += 0.6
+							else
+								SetGameplayCamRelativePitch(p + 0.016, 0.333)
+								tv += 0.1
+							end
+						until tv >= recoils[wep]
+					end
+				end
+			end
+		else
+			Wait(5000)
+		end
+		Wait(10)
+	end
+end)
+
+
+local maxSpread = 1.0
+
+CreateThread(function()
+	while true do
+		if IsPedArmed(cache.ped, 4) ~= false then
+			local ped = PlayerPedId()
+			if IsPedShooting(ped) and not IsPedDoingDriveby(ped) then
+				local _, wep = GetCurrentPedWeapon(ped)
+				_, cAmmo = GetAmmoInClip(ped, wep)
+				if recoils[wep] and recoils[wep] ~= 0 then
+					local tv = 0
+					local spread = math.random() * 2 * math.pi
+					local radius = math.sqrt(math.random()) * maxSpread
+					local xSpread = radius * math.cos(spread)
+					local ySpread = radius * math.sin(spread)
+					if GetFollowPedCamViewMode() ~= 4 then
+						repeat
+							Wait(0)
+							local p = GetGameplayCamRelativePitch()
+							SetGameplayCamRelativePitch(p + 0.1, 0.2)
+							tv = tv + 0.1
+						until tv >= recoils[wep]
+					else
+						repeat
+							Wait(0)
+							local p = GetGameplayCamRelativePitch()
+							if recoils[wep] > 0.1 then
+								SetGameplayCamRelativePitch(p + 0.6, 1.2)
+								tv = tv + 0.6
+							else
+								SetGameplayCamRelativePitch(p + 0.016, 0.333)
+								tv = tv + 0.1
+							end
+						until tv >= recoils[wep]
+					end
+					SetGameplayCamRelativeHeading(GetGameplayCamRelativeHeading() + xSpread) -- add x and y spread values to camera heading and pitch
+					SetGameplayCamRelativePitch(GetGameplayCamRelativePitch() + ySpread, 1.0)
+				end
+			end
+		else
+			Wait(1000)
+		end
+		Wait(10)
+	end
+end)
---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+lua54 'yes'
+games {'gta5'}
+client_script {
+    'client.lua',
+    'config.lua',
+}
+shared_script '@ox_lib/init.lua'
+
+
+dependency '/assetpacks'
