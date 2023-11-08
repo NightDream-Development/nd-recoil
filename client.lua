860f0a0e38bb5d4d4733239b8c5362301b421d0a
Drazox <68930610+drazoxXD@users.noreply.github.com>
2023-11-08 18:04:01 +0100

    Improve ments and other thing

--- client.lua
+++ client.lua
@@ -1,81 +1,41 @@
-local recoilEnabled = true   
-
 CreateThread(function()
 	while true do
-		if recoilEnabled then
-			if IsPedArmed(cache.ped, 4) then
-				if IsPedShooting(cache.ped) and not IsPedDoingDriveby(cache.ped) then
-					local _, wep = GetCurrentPedWeapon(cache.ped)
-					local _, cAmmo = GetAmmoInClip(cache.ped, wep)
-					
-					if Config.recoils[wep] and Config.recoils[wep] ~= 0 then
-						local tv = 0
-						local mode = GetFollowPedCamViewMode()
-						local p, step
-						
-						if mode ~= 4 then
-							step = 0.1
-							p = GetGameplayCamRelativePitch()
-						else
-							step = Config.recoils[wep] > 0.1 and 0.6 or 0.016
-							p = GetGameplayCamRelativePitch()
-						end
-						
+		if IsPedArmed(cache.ped, 4) ~= false then
+			if IsPedShooting(cache.ped) and not IsPedDoingDriveby(cache.ped) then
+				--lib.print.info(IsPedArmed(cache.ped, 4))
+				local _, wep = GetCurrentPedWeapon(cache.ped)
+				_, cAmmo = GetAmmoInClip(cache.ped, wep)
+				if Config.recoils[wep] and Config.recoils[wep] ~= 0 then
+					local tv = 0
+					local spread = math.random() * 1 * math.pi
+					local radius = math.sqrt(math.random()) * Config.maxspread
+					local xSpread = radius * math.cos(spread)
+					local ySpread = radius * math.sin(spread)
+					if GetFollowPedCamViewMode() ~= 4 then
 						repeat
 							Wait(0)
-							SetGameplayCamRelativePitch(p + step, mode ~= 4 and 0.2 or 1.2)
-							tv = tv + step
+							local p = GetGameplayCamRelativePitch()
+							SetGameplayCamRelativePitch(p + 0.1, 0.2)
+							tv = tv + 0.1
 						until tv >= Config.recoils[wep]
-					end
-				end
-			else
-				Wait(5000)
-			end
-		end
-		Wait(10)
-	end
-end)
-
--- Spread system
-CreateThread(function()
-	while true do
-		if recoilEnabled then
-			if IsPedArmed(cache.ped, 4) then
-				if IsPedShooting(cache.ped) and not IsPedDoingDriveby(cache.ped) then
-					local _, wep = GetCurrentPedWeapon(cache.ped)
-					local _, cAmmo = GetAmmoInClip(cache.ped, wep)
-					
-					if Config.recoils[wep] and Config.recoils[wep] ~= 0 then
-						local tv = 0
-						local spread = math.random() * 2 * math.pi
-						local radius = math.sqrt(math.random()) * Config.maxspread
-						local xSpread = radius * math.cos(spread)
-						local ySpread = radius * math.sin(spread)
-						
-						local mode = GetFollowPedCamViewMode()
-						local p, step
-						
-						if mode ~= 4 then
-							step = 0.1
-							p = GetGameplayCamRelativePitch()
-						else
-							step = Config.recoils[wep] > 0.1 and 0.6 or 0.016
-							p = GetGameplayCamRelativePitch()
-						end
-						
+					else
 						repeat
 							Wait(0)
-							SetGameplayCamRelativePitch(p + step, mode ~= 4 and 0.2 or 1.2)
-							tv = tv + step
+							local p = GetGameplayCamRelativePitch()
+							if Config.recoils[wep] > 0.1 then
+								SetGameplayCamRelativePitch(p + 0.6, 1.2)
+								tv = tv + 0.6
+							else
+								SetGameplayCamRelativePitch(p + 0.016, 0.333)
+								tv = tv + 0.1
+							end
 						until tv >= Config.recoils[wep]
-						
-						SetGameplayCamRelativeHeading(GetGameplayCamRelativeHeading() + xSpread)
-						SetGameplayCamRelativePitch(GetGameplayCamRelativePitch() + ySpread, 1.0)
 					end
+					SetGameplayCamRelativeHeading(GetGameplayCamRelativeHeading() + xSpread)
 				end
-			else
-				Wait(1000)
 			end
+		else
+			Wait(500)
 		end
 		Wait(10)
 	end
